0caefde4dd1a4fbabbc501c5c4faac9de6f3dd77
PavelLyalko <demon00222@gmail.com>
2026-01-13 14:29:30 +0300

    фз 15

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,17 +2,13 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
-@TestPropertySource(properties = {
-		"spring.liquibase.enabled=false",
-		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration"
-})
 @SpringBootTest
+@ActiveProfiles("test")
 class ShareItTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
